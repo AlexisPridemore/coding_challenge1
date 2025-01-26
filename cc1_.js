b3bf77e2ac3da7c9465952610aae2ddf46001e0d
Alexis Pridemore <alpridemore@usf.edu>
2025-01-25 23:36:21 -0500

    Task 2-Product Details

--- cc1_.js
+++
@@ -1,8 +0,0 @@
-// Task 1 Variables
-let employeeName = "Samantha Coop";
-const employeeID = 762;
-var isActive = true;
-
-console.log(typeof employeeName);
-console.log(typeof employeeID);
-console.log(typeof isActive);
---
+++ cc_1.js
@@ -0,0 +1,17 @@
+// Task 1 Variables
+let employeeName = "Samantha Coop";
+const employeeID = 762;
+var isActive = true;
+
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
+
+// Task 2 - Product Details
+let productName = "Laptop";
+const productPrice = 859.00;
+var isAvailable = true;
+
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvailable);
