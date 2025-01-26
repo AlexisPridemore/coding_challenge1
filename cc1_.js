64b919360bea12a2d551642b327d15948f59b6af
Alexis Pridemore <alpridemore@usf.edu>
2025-01-25 23:16:55 -0500

    Task 1-Employee Information

---
+++ cc1_.js
@@ -0,0 +1,8 @@
+// Task 1 Variables
+let employeeName = "Samantha Coop";
+const employeeID = 762;
+var isActive = true;
+
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
