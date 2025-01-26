3ddb13b02b4096da8314855b3e70763d3a12993b
Alexis Pridemore <alpridemore@usf.edu>
2025-01-25 23:45:30 -0500

    Task 3-Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,4 @@
-// Task 1 Variables
+// Task 1 Employee Information
 let employeeName = "Samantha Coop";
 const employeeID = 762;
 var isActive = true;
@@ -7,7 +7,7 @@ console.log(typeof employeeName);
 console.log(typeof employeeID);
 console.log(typeof isActive);
 
-// Task 2 - Product Details
+// Task 2 Product Details
 let productName = "Laptop";
 const productPrice = 859.00;
 var isAvailable = true;
@@ -15,3 +15,19 @@ var isAvailable = true;
 console.log(typeof productName);
 console.log(typeof productPrice);
 console.log(typeof isAvailable);
+
+// Task 3 Financial Transactions
+let accountBalance = 5000;
+
+accountBalance += 500;
+console.log(accountBalance); // 5500
+
+accountBalance -= 500;
+console.log(accountBalance); // 5000
+
+accountBalance *= 1.08;
+console.log(accountBalance); // 5400
+
+accountBalance /= 2;
+console.log(accountBalance); // 2700
+
